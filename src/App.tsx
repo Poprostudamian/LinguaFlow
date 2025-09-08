540ae6cc7a718f1954640e0a13ddf11918afa7e0
Damian Lewandowski <61431305+Poprostudamian@users.noreply.github.com>
2025-09-08 20:17:04 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+// src/App.tsx - Zaktualizowana wersja
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { AuthProvider } from './contexts/AuthContext';
+import { StudentsProvider } from './contexts/StudentsContext';
 import { Layout } from './components/Layout';
 import { RouteGuard } from './components/RouteGuard';
 import { Login } from './pages/Login';
@@ -19,47 +21,49 @@ function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <Router>
-          <div className="App">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/" element={<Navigate to="/login" replace />} />
-              
-              <Route path="/" element={<Layout />}>
-                <Route 
-                  path="student/*" 
-                  element={
-                    <RouteGuard requiredRole="student">
-                      <Routes>
-                        <Route index element={<StudentDashboard />} />
-                        <Route path="lessons" element={<StudentLessonsPage />} />
-                        <Route path="schedule" element={<StudentSchedulePage />} />
-                        <Route path="messages" element={<StudentMessagesPage />} />
-                      </Routes>
-                    </RouteGuard>
-                  } 
-                />
+        <StudentsProvider>
+          <Router>
+            <div className="App">
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/" element={<Navigate to="/login" replace />} />
                 
-                <Route 
-                  path="tutor/*" 
-                  element={
-                    <RouteGuard requiredRole="tutor">
-                      <Routes>
-                        <Route index element={<TutorDashboard />} />
-                        <Route path="students" element={<TutorStudentsPage />} />
-                        <Route path="lessons" element={<TutorLessonManagementPage />} />
-                        <Route path="messages" element={<TutorMessagesPage />} />
-                      </Routes>
-                    </RouteGuard>
-                  } 
-                />
-              </Route>
-              
-              <Route path="*" element={<Navigate to="/login" replace />} />
-            </Routes>
-          </div>
-        </Router>
+                <Route path="/" element={<Layout />}>
+                  <Route 
+                    path="student/*" 
+                    element={
+                      <RouteGuard requiredRole="student">
+                        <Routes>
+                          <Route index element={<StudentDashboard />} />
+                          <Route path="lessons" element={<StudentLessonsPage />} />
+                          <Route path="schedule" element={<StudentSchedulePage />} />
+                          <Route path="messages" element={<StudentMessagesPage />} />
+                        </Routes>
+                      </RouteGuard>
+                    } 
+                  />
+                  
+                  <Route 
+                    path="tutor/*" 
+                    element={
+                      <RouteGuard requiredRole="tutor">
+                        <Routes>
+                          <Route index element={<TutorDashboard />} />
+                          <Route path="students" element={<TutorStudentsPage />} />
+                          <Route path="lessons" element={<TutorLessonManagementPage />} />
+                          <Route path="messages" element={<TutorMessagesPage />} />
+                        </Routes>
+                      </RouteGuard>
+                    } 
+                  />
+                </Route>
+                
+                <Route path="*" element={<Navigate to="/login" replace />} />
+              </Routes>
+            </div>
+          </Router>
+        </StudentsProvider>
       </AuthProvider>
     </ThemeProvider>
   );
