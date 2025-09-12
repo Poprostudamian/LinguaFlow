5f17d522503df2a119a750c811a52c354331c998
Damian Lewandowski <61431305+Poprostudamian@users.noreply.github.com>
2025-09-12 22:57:06 +0200

    Updated StudentsContext.tsx

--- src/contexts/StudentsContext.tsx
+++ src/contexts/StudentsContext.tsx
@@ -2,6 +2,7 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { useAuth } from './AuthContext';
 import {
+  supabase,
   getTutorStudents, 
   getTutorInvitations, 
   getTutorStudentStats,
