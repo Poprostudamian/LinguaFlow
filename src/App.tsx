8db83457e41825639f217d3b6bfb63b0808eed2e
Damian Lewandowski <61431305+Poprostudamian@users.noreply.github.com>
2025-09-15 20:56:57 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { StudentDashboard } from './pages/StudentDashboard';
 import { TutorDashboard } from './pages/TutorDashboard';
 import { TutorStudentsPage } from './pages/TutorStudentsPage';
 import { TutorLessonManagementPage } from './pages/TutorLessonManagementPage';
-import { TutorSchedulePage } from './pages/TutorSchedulePage'; // ✅ NOWY IMPORT
+import { TutorSchedulePage } from './pages/TutorSchedulePage';
 import { TutorMessagesPage } from './pages/TutorMessagesPage';
 import { StudentLessonsPage } from './pages/StudentLessonsPage';
 import { StudentSchedulePage } from './pages/StudentSchedulePage';
