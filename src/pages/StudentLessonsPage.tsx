a396d18891778fc2d858a151451068a1fb1f80a4
Damian Lewandowski <61431305+Poprostudamian@users.noreply.github.com>
2025-09-15 19:10:10 +0200

    Updated StudentLessonsPage.tsx

--- src/pages/StudentLessonsPage.tsx
+++ src/pages/StudentLessonsPage.tsx
@@ -375,7 +375,6 @@ export function StudentLessonsPage() {
                       </button>
                     )}
                     
-                </div>
               </div>
             </div>
           ))}
