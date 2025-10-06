274f3416dfee37142f78d11425c677e7f0424b52
Damian Lewandowski <61431305+Poprostudamian@users.noreply.github.com>
2025-10-06 21:04:21 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function App() {
                         </Routes>
                       </RouteGuard>
                     } 
-              />
+                  />
                   
                   <Route 
                     path="tutor/*" 
