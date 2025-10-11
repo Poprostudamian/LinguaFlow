bae704cd6fdd31d25d30bcbfaef9e52596bfafb2
Damian Lewandowski <61431305+Poprostudamian@users.noreply.github.com>
2025-10-11 15:49:28 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { AuthProvider } from './contexts/AuthContext';
 import { StudentsProvider } from './contexts/StudentsContext';
 import { Layout } from './components/Layout';
 import { RouteGuard } from './components/RouteGuard';
+import { LandingPage } from './pages/LandingPage';
 import { Login } from './pages/Login';
 import { SignUp } from './pages/SignUp';
-import { LandingPage } from './pages/LandingPage';
 import { StudentDashboard } from './pages/StudentDashboard';
 import { TutorDashboard } from './pages/TutorDashboard';
 import { TutorStudentsPage } from './pages/TutorStudentsPage';
@@ -28,9 +28,9 @@ function App() {
           <Router>
             <div className="App">
               <Routes>
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
-                <Route path="/" element={<LandingPage />} />
                 
                 <Route path="/" element={<Layout />}>
                   <Route 
