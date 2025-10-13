2293a8a3e607c8a5ab67afcfd90b939dd8367811
Damian Lewandowski <61431305+Poprostudamian@users.noreply.github.com>
2025-10-13 19:25:06 +0200

    Updated StudentLessonsPage.tsx

--- src/pages/StudentLessonsPage.tsx
+++ src/pages/StudentLessonsPage.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useLanguage } from '../contexts/LanguageContext';
 import { 
   Search, 
   BookOpen, 
